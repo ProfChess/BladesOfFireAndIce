275879934bf1fe2e7fe2deb699efca794782d3df
ProfChess <122888087+ProfChess@users.noreply.github.com>
2025-09-20 22:29:44 -0400

    Final Boss Phase 1 Complete
    Added:
    - 3rd Phase 1 attack complete
    - Flame Wave now spawns, moves, and pools correctly
    
    Fixed:
    - Inconsistent pivot points in fire ball sprite caused jitter when moving

--- Assets/Bosses/Main/Attacks/FB_Attack_FlameWave.cs
+++ Assets/Bosses/Main/Attacks/FB_Attack_FlameWave.cs
@@ -6,15 +6,20 @@ public class FB_Attack_FlameWave : BaseFinalBossAttack
 {
     [Header("Attack Specs")]
     [SerializeField] private float WaveMoveSpeed = 4f;
+    [SerializeField] private float WaveMoveDuration = 3f;
     [SerializeField] private float WaveSpawnNumber = 3f;
     [SerializeField] private float WaveInbetweenTime = 1f;
 
     //Other Stats
-    private float AnimWaitTime = 0.4f;
+    private float AnimWaitTime = 0.75f;
+
+    //Quick Access
+    private GameObject Player => GameManager.Instance.getPlayer();
 
     public override void StartAttack(BossAttackOption AttackOption)
     {
         base.StartAttack(AttackOption);
+        
     }
     protected override IEnumerator SpellCastRoutine()
     {
@@ -24,12 +29,20 @@ public class FB_Attack_FlameWave : BaseFinalBossAttack
             BossAnimator.SetTrigger(SpellTrigger);
             yield return new WaitForSeconds(AnimWaitTime);
             SpawnSpell();
-            yield return new WaitForSeconds(1.1f - AnimWaitTime);
+            yield return new WaitForSeconds(WaveInbetweenTime);
         }
         AttackRoutine = null;
     }
     private void SpawnSpell()
     {
+        //Get Object
+        GameObject FlameWave = BossPoolManager.Instance.getObjectFromPool(BossAttackPrefabType.FlameWaves);
 
+        //Place Object
+        FlameWave.transform.position = gameObject.transform.position;
+        Vector2 Direction = Player.transform.position - transform.position;
+
+        //Set and Start Object
+        FlameWave.GetComponent<FlameWaveDamage>().BeginMovement(Direction, WaveMoveSpeed, WaveMoveDuration);
     }
 }
--- Assets/Bosses/Main/Attacks/VFX/WaveOfFire/FlameWaveDamage.cs
+++ Assets/Bosses/Main/Attacks/VFX/WaveOfFire/FlameWaveDamage.cs
@@ -7,6 +7,45 @@ public class FlameWaveDamage : BaseAttackDamage
     [Header("References")]
     [SerializeField] private Collider2D hitbox;
 
+    private float Speed = 1f;
+    private float Duration = 1;
 
+    //Coroutine
+    private Coroutine WaveMoveRoutine;
+
+    public void BeginMovement(Vector2 Direction, float MoveSpeed, float MoveDuration)
+    {
+        Speed = MoveSpeed;
+        Duration = MoveDuration;
+
+        //Rotation
+        float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+
+        //Start Routine
+        if (WaveMoveRoutine == null)
+        {
+            WaveMoveRoutine = StartCoroutine(WaveMove());
+        }
+    }
+    private void StopMovement()
+    {
+        gameObject.SetActive(false);
+        BossPoolManager.Instance.ReturnObjectToPool(BossAttackPrefabType.FlameWaves, gameObject);
+    }
     
+    private IEnumerator WaveMove()
+    {
+        Debug.Log("Coroutine Started");
+        float timepassed = 0f;
+        while (timepassed < Duration)
+        {
+            timepassed += Time.deltaTime;
+            transform.Translate(Vector2.up * Speed * Time.deltaTime);
+            yield return null;
+        }
+
+        StopMovement();
+        WaveMoveRoutine = null;
+    }
 }
