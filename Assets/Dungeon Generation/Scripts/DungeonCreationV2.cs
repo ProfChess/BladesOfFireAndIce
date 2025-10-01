151682552c91a7e8a5153a3a61755b051bab20ef
ProfChess <122888087+ProfChess@users.noreply.github.com>
2025-10-01 11:33:26 -0400

    Room Creation Buffer
    Added:
    - Rooms Now Account for Buffer spacing around edges
    
    Fixed:
    - Dungeon Creation script was not adding rooms into the list correctly, and it has the splitting axis logic reversed

--- Assets/Dungeon Generation/Scripts/DungeonCreationV2.cs
+++ Assets/Dungeon Generation/Scripts/DungeonCreationV2.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 public class DungeonCreationV2 : MonoBehaviour
 {
     //Specific Stats
-    public RectInt TotalDungeonSize = new RectInt(0, 0, 100, 100);
-    private int MinRoomLength = 10;
-    private int MinRoomHeight = 10;
+    public RectInt TotalDungeonSize = new RectInt(0, 0, 65, 65);
+    private int MinRoomLength = 8;
+    private int MinRoomHeight = 8;
+    private int RoomBuffer = 2;
     private enum SplitAxis { Horizontal, Vertical, Invalid }
 
+    //Rooms 
     private SingleDungeonRoom StartingRoom;
     private List<SingleDungeonRoom> DungeonRooms = new List<SingleDungeonRoom>();
 
     private void Awake()
     {
+        MinRoomHeight = MinRoomHeight + RoomBuffer * 2;
+        MinRoomLength = MinRoomLength + RoomBuffer * 2; 
         StartingRoom = new SingleDungeonRoom(TotalDungeonSize);
         SplitSpace(StartingRoom);
+        for (int i = 0; i < DungeonRooms.Count; i++)
+        {
+            Debug.Log("Room #" + i + " Width: " +  DungeonRooms[i].Area.width + " Height: " + DungeonRooms[i].Area.height);
+        }
     }
     private void SplitSpace(SingleDungeonRoom Room)
     {
@@ -34,15 +42,15 @@ public class DungeonCreationV2 : MonoBehaviour
         }
         else if (longEnough && !tallEnough)
         {
-            AxisToSplit = SplitAxis.Horizontal;
+            AxisToSplit = SplitAxis.Vertical;
         }
         else if (!longEnough && tallEnough)
         {
-            AxisToSplit = SplitAxis.Vertical;
+            AxisToSplit = SplitAxis.Horizontal;
         }
-        else if (!longEnough && !tallEnough)
+        else
         {
-            DungeonRooms.Add(Room);
+            AddRoomToList(Room);
             return;
         }
 
@@ -57,30 +65,43 @@ public class DungeonCreationV2 : MonoBehaviour
         switch (Axis)
         {
             case SplitAxis.Horizontal:
-                int YSplit = Room.Area.height / 2;
+                int minYSplitPoint = Room.Area.yMin + MinRoomHeight;
+                int maxYSplitPoint = Room.Area.yMax - MinRoomHeight;
+
+                //Pick Random Split
+                int YSplit = Room.Area.yMin + Room.Area.height / 2;
                 if (Room.Area.height != MinRoomHeight * 2)
                 {
-                    YSplit = Random.Range(Room.Area.yMin + MinRoomHeight, Room.Area.yMax - MinRoomHeight);
+                    YSplit = Random.Range(minYSplitPoint, maxYSplitPoint + 1);
                 }
 
+
                 //Create Top and Bottom Rooms
-                SingleDungeonRoom TopRoom = new SingleDungeonRoom(new RectInt(Room.Area.xMin, Room.Area.yMin, Room.Area.width, YSplit - Room.Area.yMin));
-                SingleDungeonRoom BottomRoom = new SingleDungeonRoom(new RectInt(Room.Area.xMin, YSplit, Room.Area.width, Room.Area.yMax - YSplit));
+                RectInt TopRoomRect = new RectInt(Room.Area.xMin, Room.Area.yMin, Room.Area.width, YSplit - Room.Area.yMin);
+                RectInt BotRoomRect = new RectInt(Room.Area.xMin, YSplit, Room.Area.width, Room.Area.yMax - YSplit);
+                SingleDungeonRoom TopRoom = new SingleDungeonRoom(TopRoomRect);
+                SingleDungeonRoom BottomRoom = new SingleDungeonRoom(BotRoomRect);
                 
                 //Assigning Children
                 Room.AssignChildren(TopRoom, BottomRoom);
                 break;
             
             case SplitAxis.Vertical:
-                int XSplit = Room.Area.width / 2;
+                int minXSplitPoint = Room.Area.xMin + MinRoomLength;
+                int maxXSplitPoint = Room.Area.xMax - MinRoomLength;
+
+                //Pick Random Point
+                int XSplit = Room.Area.xMin + Room.Area.width / 2;
                 if (Room.Area.width != MinRoomLength * 2)
                 {
-                    XSplit = Random.Range(Room.Area.xMin + MinRoomLength, Room.Area.xMax - MinRoomLength);
+                    XSplit = Random.Range(minXSplitPoint, maxXSplitPoint + 1);
                 }
 
                 //Create Left and Right
-                SingleDungeonRoom LeftRoom = new SingleDungeonRoom(new RectInt(Room.Area.xMin, Room.Area.yMin, XSplit - Room.Area.xMin, Room.Area.height));
-                SingleDungeonRoom RightRoom = new SingleDungeonRoom(new RectInt(XSplit, Room.Area.yMin, Room.Area.xMax - XSplit, Room.Area.height));
+                RectInt LeftRoomRect = new RectInt(Room.Area.xMin, Room.Area.yMin, XSplit - Room.Area.xMin, Room.Area.height);
+                RectInt RightRoomRect = new RectInt(XSplit, Room.Area.yMin, Room.Area.xMax - XSplit, Room.Area.height);
+                SingleDungeonRoom LeftRoom = new SingleDungeonRoom(LeftRoomRect);
+                SingleDungeonRoom RightRoom = new SingleDungeonRoom(RightRoomRect);
                 
                 //Assigning Children
                 Room.AssignChildren(LeftRoom, RightRoom);
@@ -90,6 +111,37 @@ public class DungeonCreationV2 : MonoBehaviour
                 Debug.Log("Undefined Axis Given");
                 break;
         }
+    }
+
+    private void AddRoomToList(SingleDungeonRoom Room)
+    {
+
+        RectInt NewSpace = new RectInt(
+            Room.Area.xMin + RoomBuffer,
+            Room.Area.yMin + RoomBuffer,
+            Room.Area.width - RoomBuffer*2,
+            Room.Area.height - RoomBuffer*2);
+
+        Room.Area = NewSpace;
+        DungeonRooms.Add(Room);
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        foreach (SingleDungeonRoom room in DungeonRooms)
+        {
+            Vector3 size = new Vector3(room.Area.width, room.Area.height, 0);
+            Vector3 center = new Vector3(room.Area.x + room.Area.width * 0.5f, room.Area.y + room.Area.height * 0.5f, 0);
+
+            Gizmos.DrawWireCube(center, size);
+        }
+        Gizmos.color = Color.red;
+        Vector3 totalSize = new Vector3(TotalDungeonSize.width, TotalDungeonSize.height);
+        Vector3 totalcenter = new Vector3(
+            TotalDungeonSize.x + TotalDungeonSize.width * 0.5f,
+            TotalDungeonSize.y + TotalDungeonSize.height * 0.5f, 0);
+        Gizmos.DrawWireCube(totalcenter, totalSize);
     }
 }
 
